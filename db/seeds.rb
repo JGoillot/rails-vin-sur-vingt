479a6e9496b0ea4775e6af9b8bb3fcf87b7c2469
JFachetti <goillot.jeremy@gmail.com>
2016-08-16 17:25:03 +0200

    Improving dependant and finish the seed with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts 'Seed Destroy User'
+
+# # User.destroy_all
+require 'faker'
+
+puts '- Seed Delete all User'
+
+User.destroy_all
+
+
+puts '- Seed Create User'
+
+kind = %w( fridge rock underground )
+
+10.times do
+  puts '- Create new User'
+
+  user = User.create(
+    email: Faker::Internet.email,
+    password: "123soleil",
+    firstname: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
+    birthdate: Date.today
+  )
+  if [true, false].sample
+    puts '- Seed Create Cellar'
+    user.cellars.create(
+      description: Faker::ChuckNorris.fact,
+      address: Faker::Address.city,
+      space_available: Faker::Number.number(2),
+      price_per_month: Faker::Number.decimal(2),
+      kind: kind.sample
+    )
+  end
+
+end
+
+puts '= Seed Create User Done'
