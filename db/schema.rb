8790441736979077a5dc1ef303c2a15612eca672
PierreVincent Taisne <pvtaisne@gmail.com>
2016-08-16 18:59:54 +0200

    replaced firstname and lastname by first_name and last_name

--- db/schema.rb
+++ db/schema.rb
@@ -84,8 +84,8 @@ ActiveRecord::Schema.define(version: 20160816151127) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "firstname"
-    t.string   "lastname"
+    t.string   "first_name"
+    t.string   "last_name"
     t.date     "birthdate"
     t.string   "provider"
     t.string   "uid"
