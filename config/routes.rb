35ec9f1e5a44a806030b0af06b11c1b2b1f2e228
PierreVincent Taisne <pvtaisne@gmail.com>
2016-08-16 18:45:47 +0200

    add facebook connect to sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   get 'conversations/show'
 
-  devise_for :users
-  root to: 'pages#home'
-
+  # devise_for :users
+  # root to: 'pages#home'
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    root to: 'pages#home'
   resources :cellars, only: [:index, :show] do
     resources :reservations, only: [:create, :new, :destroy]
     resources :messages, only: [:create, :new]
