bb4cf8c3101d75724082af4f497f97111d6e188b
PierreVincent Taisne <pvtaisne@gmail.com>
2016-08-17 15:38:31 +0200

    size to dimension finished

--- db/migrate/20160815163456_create_bottle_lots.rb
+++ db/migrate/20160815163456_create_bottle_lots.rb
@@ -1,7 +1,7 @@
 class CreateBottleLots < ActiveRecord::Migration[5.0]
   def change
     create_table :bottle_lots do |t|
-      t.string :size
+      t.string :dimension
       t.integer :year
       t.string :color
       t.string :appelation
