18b82706b4beb2c27ff8ceacd87ae96332d4f601
PierreVincent Taisne <pvtaisne@gmail.com>
2016-08-17 17:13:03 +0200

    display all messages in message/index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,34 @@
 class MessagesController < ApplicationController
+
+  before_action :find_cellar, only: [ :new, :create ]
   def create
+   @message = Message.new(message_params)
+   @message.cellar = @cellar
+   @message.user = current_user
+    if @message.save
+      redirect_to user_dashboard_path
+    else
+      render :new
+    end
   end
 
   def new
+    @message = Message.new
+  end
+
+  def index
+    user = current_user
+    @message = Message.where(user_id: user.id)
+  end
+
+  private
+
+  def find_cellar
+    @cellar = Cellar.find(params[:cellar_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 
 end
