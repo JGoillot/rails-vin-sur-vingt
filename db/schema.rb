a7e8f408cc6fe1b445a1af76091a0a4058286b63
Charly Martin <charly.martin.1207@gmail.com>
2016-08-18 11:18:15 +0200

    Added: latitude & longitude to Cellars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817143703) do
+ActiveRecord::Schema.define(version: 20160818091659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20160817143703) do
     t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_cellars_on_user_id", using: :btree
   end
 
@@ -99,12 +101,14 @@ ActiveRecord::Schema.define(version: 20160817143703) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "firstname"
+    t.string   "lastname"
     t.date     "birthdate"
     t.string   "provider"
     t.string   "uid"
     t.string   "facebook_picture_url"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
