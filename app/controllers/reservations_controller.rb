cd36da7064d9d8843f5b18c651d5e0e04b3f9cf8
Florian Barbato <flobarbato@gmail.com>
2016-08-18 12:40:52 +0200

    you can now book a reservation. Now I need to add bottle to reservations and render a page which centralize reservations for admin

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,26 @@
 class ReservationsController < ApplicationController
   def new
+    @cellar = Cellar.find(params[:cellar_id])
+    @reservation = Reservation.new
     @user = current_user
+    @bottle_lots = current_user.bottle_lots
   end
 
   def create
+    @cellar = Cellar.find(params[:cellar_id])
+    @user = current_user
+    @reservation = Reservation.new(reservation_params)
+    @reservation.save
+    redirect_to cellar_path(@cellar)
   end
 
   def destroy
   end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:check_in, :check_out)
+  end
+
 end
